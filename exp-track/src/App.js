52148587c12e149616f21f667eeb3be15dd06e17
Abayomi <abiyo4reel@gmail.com>
2022-08-30 07:12:18 -0700

    First Commit

---
+++ exp-track/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './App.css';
+
+import ExpenseTracker from "./components/ExpenseTracker";
+
+function App() {
+    return <ExpenseTracker></ExpenseTracker>
+}
+
+export default App;
